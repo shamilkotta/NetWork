5854b9d9dd3c711b65dfbb1d46e53034a2b9661e
shamilkotta <shamilkotta99@gmail.com>
2022-11-04 11:15:01 +0530

    error bug fix

--- middlewares/errorHandler.js
+++ middlewares/errorHandler.js
@@ -1,5 +1,5 @@
 module.exports = (err, req, res, next) => {
-  let statusCode;
+  let statusCode = 500;
   if (err.message === "Not found") statusCode = 404;
   res.status(statusCode).send(err);
   next();
