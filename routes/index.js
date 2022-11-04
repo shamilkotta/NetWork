d0f50cd0bdc1eea5a532ce1e298a23d1c169a645
shamilkotta <shamilkotta99@gmail.com>
2022-11-04 11:24:25 +0530

    basic routes added

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,16 @@ router.get("/", (req, res) => {
   res.render("home");
 });
 
+router.get("/login", (req, res) => {
+  res.render("login");
+});
+
+router.get("/signup", (req, res) => {
+  res.render("signup");
+});
+
+router.get("/profile", (req, res) => {
+  res.render("profile");
+});
+
 module.exports = router;
