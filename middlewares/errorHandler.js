03bf37a2aa4b43be3dd4ad7c8f049577bf74c61b
shamilkotta <shamilkotta99@gmail.com>
2022-11-04 11:35:26 +0530

    error page added

--- middlewares/errorHandler.js
+++ middlewares/errorHandler.js
@@ -1,6 +1,6 @@
+// eslint-disable-next-line no-unused-vars
 module.exports = (err, req, res, next) => {
   let statusCode = 500;
   if (err.message === "Not found") statusCode = 404;
-  res.status(statusCode).send(err);
-  next();
+  res.status(statusCode).render("404");
 };
