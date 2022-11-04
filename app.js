6636f4923fb07136f2346f88d6440d5407b31df5
shamilkotta <shamilkotta99@gmail.com>
2022-11-04 10:28:33 +0530

    basic route setup

--- app.js
+++ app.js
@@ -1,9 +1,29 @@
 const express = require("express");
+const cookieParser = require("cookie-parser");
+const logger = require("morgan");
+const errorHandler = require("./middlewares/errorHandler");
+require("dotenv").config();
 
 const app = express();
+
+// logger
+app.use(logger("dev"));
+
+// parsers
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
 
 app.get("/", (req, res) => {
   res.send("Hi welcome");
 });
+
+// 404
+app.use((req, res, next) => {
+  next(new Error("Not found"));
+});
+
+// error handler
+app.use(errorHandler);
 
 module.exports = app;
