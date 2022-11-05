446c0fd505048b96129a7610671a3985ebc41fef
shamilkotta <shamilkotta99@gmail.com>
2022-11-05 20:24:14 +0530

    guest verification added

--- middlewares/authorization.js
+++ middlewares/authorization.js
@@ -2,3 +2,8 @@ module.exports.verifyLogin = (req, res, next) => {
   if (req.session.logedIn) next();
   else res.redirect("/login");
 };
+
+module.exports.verifyGuest = (req, res, next) => {
+  if (req.session.logedIn) res.redirect("/");
+  else next();
+};
