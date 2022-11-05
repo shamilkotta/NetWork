f692e10a6dd30b47bf22bba949374a3bc83cbef8
shamilkotta <shamilkotta99@gmail.com>
2022-11-05 14:46:27 +0530

    controller created

---
+++ controllers/index.js
@@ -0,0 +1,71 @@
+const { validationResult, matchedData } = require("express-validator");
+
+const { getAllUsers, loginHelper, signupHelper } = require("../helpers");
+
+module.exports = {
+  getHome: (req, res) => {
+    if (req.session.logedIn) {
+      getAllUsers()
+        .then((users) => {
+          res.render("home", {
+            users,
+            helpers: {
+              avatar: (name) => name.split("")[0],
+            },
+          });
+        })
+        .catch((err) => {
+          console.error(err);
+          res.render("home", { users: [] });
+        });
+    } else res.redirect("/login");
+  },
+
+  postLogin: (req, res) => {
+    const err = validationResult(req).array();
+    if (err.length > 0) res.render("login", { error: err[0].msg });
+    else {
+      const data = matchedData(req, {
+        onlyValidData: true,
+        includeOptionals: false,
+      });
+      loginHelper(data)
+        .then((response) => {
+          if (response.success) {
+            req.session.logedIn = true;
+            req.session.admin = response.admin;
+            req.session.id = data.email;
+            res.redirect("/");
+          } else res.render("login", { error: response.message });
+        })
+        .catch((error) => {
+          console.error(error);
+          res.render("login", { error: "Something went wrong, try again" });
+        });
+    }
+  },
+
+  postSignup: (req, res) => {
+    const err = validationResult(req).array();
+    if (err.length > 0) res.render("signup", { error: err[0].msg });
+    else {
+      const data = matchedData(req, {
+        onlyValidData: true,
+        includeOptionals: false,
+      });
+      signupHelper(data)
+        .then((response) => {
+          if (response.success) {
+            req.session.logedIn = true;
+            req.session.admin = false;
+            req.session.id = data.email;
+            res.redirect(303, "/");
+          } else res.render("signup", { error: response.message });
+        })
+        .catch((error) => {
+          console.error(error);
+          res.render("signup", { error: "Something went wrong, try again" });
+        });
+    }
+  },
+};
--- routes/index.js
+++ routes/index.js
@@ -1,87 +1,26 @@
 const express = require("express");
-const { validationResult, matchedData } = require("express-validator");
 
 const loginValidation = require("../middlewares/validators/loginValidation");
 const signupValidation = require("../middlewares/validators/signupValidation");
-const { signupHelper, loginHelper, getAllUsers } = require("../helpers");
+const { getHome, postLogin, postSignup } = require("../controllers");
 
 const router = express.Router();
 
-router.get("/", (req, res) => {
-  if (req.session.logedIn) {
-    getAllUsers()
-      .then((users) => {
-        res.render("home", {
-          users,
-          helpers: {
-            avatar: (name) => name.split("")[0],
-          },
-        });
-      })
-      .catch((err) => {
-        console.error(err);
-        res.render("home", { users: [] });
-      });
-  } else res.redirect("/login");
-});
+router.get("/", getHome);
 
 router.get("/login", (req, res) => {
   if (req.session.logedIn) res.redirect("/");
   else res.render("login");
 });
 
-router.post("/login", loginValidation, (req, res) => {
-  const err = validationResult(req).array();
-  if (err.length > 0) res.render("login", { error: err[0].msg });
-  else {
-    const data = matchedData(req, {
-      onlyValidData: true,
-      includeOptionals: false,
-    });
-    loginHelper(data)
-      .then((response) => {
-        if (response.success) {
-          req.session.logedIn = true;
-          req.session.admin = response.admin;
-          req.session.id = data.email;
-          res.redirect("/");
-        } else res.render("login", { error: response.message });
-      })
-      .catch((error) => {
-        console.error(error);
-        res.render("login", { error: "Something went wrong, try again" });
-      });
-  }
-});
+router.post("/login", loginValidation, postLogin);
 
 router.get("/signup", (req, res) => {
   if (req.session.logedIn) res.redirect("/");
   else res.render("signup");
 });
 
-router.post("/signup", signupValidation, (req, res) => {
-  const err = validationResult(req).array();
-  if (err.length > 0) res.render("signup", { error: err[0].msg });
-  else {
-    const data = matchedData(req, {
-      onlyValidData: true,
-      includeOptionals: false,
-    });
-    signupHelper(data)
-      .then((response) => {
-        if (response.success) {
-          req.session.logedIn = true;
-          req.session.admin = false;
-          req.session.id = data.email;
-          res.redirect(303, "/");
-        } else res.render("signup", { error: response.message });
-      })
-      .catch((error) => {
-        console.error(error);
-        res.render("signup", { error: "Something went wrong, try again" });
-      });
-  }
-});
+router.post("/signup", signupValidation, postSignup);
 
 router.get("/profile", (req, res) => {
   res.render("profile");
