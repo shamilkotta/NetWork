998d3cafcfa294939da399331c648891e0f39f96
shamilkotta <shamilkotta99@gmail.com>
2022-11-06 13:12:23 +0530

    admin router set

--- routes/admin.js
+++ routes/admin.js
@@ -1,9 +1,21 @@
 const express = require("express");
 
+const { getAllUsers } = require("../helpers/index");
+
 const router = express.Router();
 
 router.get("/", (req, res) => {
-  res.send("admin route");
+  getAllUsers()
+    .then((users) => {
+      res.render("admin/index", {
+        users,
+        user: req.session.user,
+      });
+    })
+    .catch((err) => {
+      console.error(err);
+      res.render("admin/index", { users: [], user: req.session.user });
+    });
 });
 
 module.exports = router;
